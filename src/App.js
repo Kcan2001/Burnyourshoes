e10577c9d318a3b95677b3dc6905e0feb4257de0
Kcan2001 <kyleacannon@gmail.com>
2022-06-30 11:05:22 -0700

    main

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img alt="burn shoes" src="https://as1.ftcdn.net/v2/jpg/02/91/87/76/1000_F_291877613_2tWnTlO1g5qfj36FO3ZZaivdzw4oUNXN.jpg"  />
+        <a
+          className="App-link"
+          href="https://youtu.be/dQw4w9WgXcQ"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Click here to burn your shoes.
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
